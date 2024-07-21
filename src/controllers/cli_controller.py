1a4195a5fc9544d3b4abe6ff40c8ddf27c5df2e1
theresaxn <hongoanh3@hotmail.com>
2024-07-21 23:20:19 +1000

    added servers in seed command

--- src/controllers/cli_controller.py
+++ src/controllers/cli_controller.py
@@ -1,7 +1,10 @@
+from datetime import date
+
 from flask import Blueprint
 
 from main import db, bcrypt
 from models.user import User
+from models.server import Server
 
 db_commands = Blueprint("db", __name__)
 
@@ -22,17 +25,39 @@ def seed_table():
             username = "user1",
             email = "user1@email.com",
             password = bcrypt.generate_password_hash("123456").decode("utf-8"),
-            name = "User 1",
+            name = "user 1",
             status = "online"
         ),
         User(
             username = "user2",
             email = "user2@email.com",
             password = bcrypt.generate_password_hash("123456").decode("utf-8"),
-            name = "User 2",
+            name = "user 2",
             status = "offline"
         )
     ]
+
     db.session.add_all(users)
+
+    servers = [
+        Server(
+            server_name = "server 1",
+            created_on = date.today(),
+            user = users[0]
+        ),
+        Server(
+            server_name = "server 2",
+            created_on = date.today(),
+            user = users[0]
+        ),
+        Server(
+            server_name = "server a",
+            created_on = date.today(),
+            user = users[1]
+        )
+    ]
+
+    db.session.add_all(servers)
+
     db.session.commit()
     print("tables seeded")
